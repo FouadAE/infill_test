f65c29f2b3bcb4de1cb74195bacae5a892fd287d
FouadAE <fouad.aitelhaj@edu.uca.ma>
2023-05-15 23:38:14 +0100

    modify the engine 1

--- utils/extract_properties.py
+++ utils/extract_properties.py
@@ -9,41 +9,32 @@ OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 
 openai.api_key = OPENAI_API_KEY
 
+import json
 
 def extract_properties(text):
     # Initialize the output data
     output_data = {"properties": []}
+
     # Create prompt
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "user", "content": "Extract the properties from the following text " +
+            {"role": "user", "content": "Extract the properties from the following text: '" +
 
-             "Here is the  text" +
-             str(text) + "the output should be a JSON-formatted string with the following format: " + str(output_data)
+             str(text) + "'. Please respond with a JSON-formatted string with the following format: " + json.dumps(output_data)
              },
         ],
         temperature=0.9,
     )
-    # response = openai.ChatCompletion.create(
-    #     engine="gpt-4",
-    #     prompt="Extract the properties from the following text " +
-    #          "Here is the  text" +
-    #        str(text) + "the output should be a JSON-formatted string with the following format: " + str(output_data)
-    #           ,
-    #       # You can specify a stopping condition if needed
-    #     temperature=0.2,  # Adjust the temperature to control the randomness of the output
-        
-        
-    # )
-    # Get the properties from the response
-    #properties = response.choices[0]
-    # Parse the response and return the mapping
-    #output_data = json.loads(properties)
-
-    #return properties
     response = response.choices[0].message.content
-    output_data = json.loads(response)
+
+    try:
+        # Attempt to load the response as JSON
+        output_data = json.loads(response)
+    except json.JSONDecodeError:
+        # If the response isn't valid JSON, return a default value or handle the error
+        print("Invalid JSON received:", response)
+        return None
+
     print(response)
     return output_data
-
