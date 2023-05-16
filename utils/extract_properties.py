aa9722c76883ef71ad71f3dfe0ef735c706459c2
FouadAE <fouad.aitelhaj@edu.uca.ma>
2023-05-16 20:22:44 +0100

    sd

--- utils/extract_properties.py
+++ utils/extract_properties.py
@@ -11,7 +11,6 @@ OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 openai.api_key = OPENAI_API_KEY
 
 import json
-
 def extract_properties(text):
     # Initialize the output data
     output_data = {"properties": []}
@@ -20,27 +19,24 @@ def extract_properties(text):
     # Create prompt
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        #batch_size=10,
         temperature=0.5,
         messages=[
             {"role": "user", "content": "Extract the properties from the following text: '" +
-
              str(text) + "'. Please respond with a JSON-formatted string with the following format: " + json.dumps(output_data)
              },
         ],
     )
-    response = response.choices[0].message.content
-    # #end time
-    # end_time = time.time()
-    # #print time
-    # print("Time taken to extract properties: " + str(end_time - start_time))
-    # try:
-    #     # Attempt to load the response as JSON
-    #     output_data = json.loads(response)
-    # except json.JSONDecodeError:
-    #     # If the response isn't valid JSON, return a default value or handle the error
-    #     print("Invalid JSON received:", response)
-    #     return None
+    response_content = response.choices[0].message.content
+    #end time
+    end_time = time.time()
+    #print time
+    print("Time taken to extract properties: " + str(end_time - start_time))
+    try:
+        # Attempt to load the response content as JSON
+        output_data = json.loads(response_content)
+    except json.JSONDecodeError:
+        # If the response isn't valid JSON, return a default value or handle the error
+        print("Invalid JSON received:", response_content)
+        return None
 
-    # print(response)
-    return response
+    return output_data
